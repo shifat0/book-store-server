9211c59016330c0aef85e8f36dcd40f9d47ce374
shifat0 <sifuisu3@gmail.com>
2024-07-30 00:46:00 +0600

    booksModel modfied

--- src/models/booksModel.ts
+++ src/models/booksModel.ts
@@ -1,17 +1,19 @@
 import db from '../db';
 
 db.schema
-  .createTableIfNotExists('authors', (table) => {
+  .createTableIfNotExists('books', (table) => {
     table.increments('id').primary();
-    table.string('name').notNullable();
-    table.text('bio');
-    table.date('birthdate').notNullable();
+    table.string('title').notNullable();
+    table.text('description');
+    table.date('published_date').notNullable();
+    table.integer('author_id').notNullable();
+    table.foreign('author_id').references('id').inTable('authors');
   })
   .then(() => {
-    console.log('Authors table created');
+    console.log('Books table created');
   })
-  .catch((error) => {
-    console.error('Error creating authors table:', error);
+  .catch((err) => {
+    console.error('Error creating books table:', err);
   })
   .finally(() => {
     db.destroy();
